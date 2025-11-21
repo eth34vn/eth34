e8959544fc0e598c38a6ac91e4d4ae15d8d7ac1d
ZxStim <zxstim@gmail.com>
2025-11-20 23:19:22 -0300

    feat: updated videos

--- data/videos.ts
+++ data/videos.ts
@@ -25,4 +25,14 @@ export const videos = [
     title: "Hành trình xây dựng cầu nối cho internet của các blockchain | Phúc từ Concero",
     src: "https://www.youtube.com/embed/X4bIpWHs4vs?si=yD8a_geD1HQzGcxh",
   },
+  {
+    id: 6,
+    title: "Khởi Nghiệp Cơ Sở Hạ Tầng Xác Minh Giao Dịch Cho Blockchain | Susan từ OriginStake",
+    src: "https://www.youtube.com/embed/aH4WQJ1l8SA?si=Omrbb7_7My4B90p5",
+  },
+  {
+    id: 7,
+    title: "Bắt Đầu Sự Nghiệp Kỹ Thuật Trong Ngành Tài Sản Số | Workshop tại Đại Học Công Nghệ Thông Tin",
+    src: "https://www.youtube.com/embed/JaL3SJcnK8g?si=K66BGsu8SLXw7oSN",
+  }
 ];
