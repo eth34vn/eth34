f0d2b28c1a2323f67700d5d000032c920cc9f2ed
ZxStim <zxstim@gmail.com>
2025-12-09 14:18:59 +0700

    feat: updated contents

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -15,5 +15,40 @@ export function baseOptions(): BaseLayoutProps {
         />
       ),
     },
+    links: [
+      {
+        type: 'icon',
+        icon: <Image src="/facebook.svg" alt="Facebook" className="dark:invert" width={20} height={20} />,
+        text: 'Facebook',
+        url: 'https://www.facebook.com/defivn',
+        // secondary items will be displayed differently on navbar
+        secondary: true,
+
+      },
+      {
+        type: 'icon',
+        icon: <Image src="/youtube.svg" alt="Youtube" className="dark:invert" width={20} height={20} />,
+        text: 'Youtube',
+        url: 'https://www.youtube.com/@defivn',
+        // secondary items will be displayed differently on navbar
+        secondary: true,
+      },
+      {
+        type: 'icon',
+        icon: <Image src="/x.svg" alt="X" className="dark:invert" width={20} height={20} />,
+        text: 'X',
+        url: 'https://x.com/defivnx',
+        // secondary items will be displayed differently on navbar
+        secondary: true,
+      },
+      {
+        type: 'icon',
+        icon: <Image src="/github.svg" alt="Github" className="dark:invert" width={20} height={20} />,
+        text: 'Github',
+        url: 'https://github.com/defivn',
+        // secondary items will be displayed differently on navbar
+        secondary: true,
+      },
+    ],
   };
 }
