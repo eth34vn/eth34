b03989382f4e6f5eaadeacba6401920062de3258
ZxStim <zxstim@gmail.com>
2025-12-25 14:59:23 +0700

    feat: updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,17 +12,17 @@ const fontVietnamese = Be_Vietnam_Pro({
 });
 
 export const metadata: Metadata = {
-  title: "DeFi.vn",
-  description: "Bách khoa toàn thư về tài chính phi tập trung",
-  metadataBase: new URL("https://www.defi.vn"),
+  title: "ETH34",
+  description: "Bách khoa toàn thư về Ethereum",
+  metadataBase: new URL("https://www.eth34.vn"),
   openGraph: {
-    title: "DeFi.vn",
-    description: "Bách khoa toàn thư về tài chính phi tập trung",
-    url: "https://www.defi.vn",
-    siteName: "DeFi.vn",
+    title: "ETH34",
+    description: "Bách khoa toàn thư về Ethereum",
+    url: "https://www.eth34.vn",
+    siteName: "ETH34",
     images: [
       {
-        url: "/defi-vn-tbn.png",
+        url: "/eth34vn-tbn.png",
         width: 1200,
         height: 630,
         alt: "og-image",
@@ -33,10 +33,10 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "DeFi.vn",
-    description: "Bách khoa toàn thư về tài chính phi tập trung",
-    creator: "@zxstim",
-    images: ["/defi-vn-tbn.png"],
+    title: "ETH34",
+    description: "Bách khoa toàn thư về Ethereum",
+    creator: "@eth34vn",
+    images: ["/eth34vn-tbn.png"],
   },
 };
 
@@ -46,7 +46,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
       <Script
         defer
         src="https://analytics.zxstim.com/script.js"
-        data-website-id="52d241bb-9bb1-4881-bcea-589f7eddbf79"
+        data-website-id="626fe135-90aa-480c-bdfe-f6f95df981ee"
       />
       <body className="flex flex-col min-h-screen">
         <RootProvider>
