3403df6e0667fb56d42cbada59c4868c5e51c559
ZxStim <zxstim@gmail.com>
2025-12-25 14:56:27 +0700

    feat: updated layouts

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -7,8 +7,8 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <Image
-          src="/hero.svg"
-          alt="DeFi.vn"
+          src="/eth34vn-full.svg"
+          alt="ETH34"
           width={100}
           height={30}
           className="dark:invert"
@@ -35,7 +35,7 @@ export function baseOptions(): BaseLayoutProps {
         type: 'icon',
         icon: <Image src="/facebook.svg" alt="Facebook" className="dark:invert" width={20} height={20} />,
         text: 'Facebook',
-        url: 'https://www.facebook.com/defivn',
+        url: 'https://www.facebook.com/eth34vn',
         // secondary items will be displayed differently on navbar
         secondary: true,
       },
@@ -43,7 +43,7 @@ export function baseOptions(): BaseLayoutProps {
         type: 'icon',
         icon: <Image src="/youtube.svg" alt="Youtube" className="dark:invert" width={20} height={20} />,
         text: 'Youtube',
-        url: 'https://www.youtube.com/@defivn',
+        url: 'https://www.youtube.com/@eth34vn',
         // secondary items will be displayed differently on navbar
         secondary: true,
       },
@@ -51,7 +51,7 @@ export function baseOptions(): BaseLayoutProps {
         type: 'icon',
         icon: <Image src="/x.svg" alt="X" className="dark:invert" width={20} height={20} />,
         text: 'X',
-        url: 'https://x.com/defivnx',
+        url: 'https://x.com/eth34vn',
         // secondary items will be displayed differently on navbar
         secondary: true,
       },
@@ -59,7 +59,7 @@ export function baseOptions(): BaseLayoutProps {
         type: 'icon',
         icon: <Image src="/github.svg" alt="Github" className="dark:invert" width={20} height={20} />,
         text: 'Github',
-        url: 'https://github.com/defivn',
+        url: 'https://github.com/eth34vn',
         // secondary items will be displayed differently on navbar
         secondary: true,
       },
